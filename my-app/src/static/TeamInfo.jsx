207ba0a038ce25f3515443df05ccf8ffd0f3b1b8
Adrian Sanchez <adriansan@Adrians-MacBook-Air.local>
2023-02-14 19:13:07 -0600

    About page skeleton. Gitlab API integration. Team info. Dependencies.

--- my-app/src/static/TeamInfo.jsx
+++ my-app/src/static/TeamInfo.jsx
@@ -1,63 +1,61 @@
-import ColeAvatar from "../assets/avatars/cole.png";
-import FaizaAvatar from "../assets/avatars/faiza.png";
-import MikalaAvatar from "../assets/avatars/mikala.png";
-import SarthakAvatar from "../assets/avatars/sarthak.png";
-import ThomasAvatar from "../assets/avatars/thomas.png";
-
-// inspired by TexasVotes project. https://gitlab.com/forbesye/fitsbits/-/blob/master/front-end/src/views/About/AboutInfo.js
+// import SumayaAvatar from "../assets/avatars/sumaya.png";
+// import PeytonAvatar from "../assets/avatars/peyton.png";
+// import PeterAvatar from "../assets/avatars/peter.png";
+// import NiyatiAvatar from "../assets/avatars/niyati.png";
+// import AdrianAvatar from "../assets/avatars/adrian.png";
 
 const teamInfo = [
   {
-    name: "Cole Amlong",
-    gitlab_username: "coleamlong",
-    email: "coleamlong@gmail.com",
-    image: ColeAvatar,
-    role: "Front-end",
-    bio: "I'm a third year student at the University of Texas at Austin pursuing a BS in computer sciecne and a minor in business. I love to cook declicous food and discover new music. Catch me on the weekends DJing events!",
+    name: "Sumaya Al-Bedaiwi",
+    gitlab_username: "salbedaiwi",
+    email: "email",
+    // image: SumayaAvatar,
+    role: "Fullstack",
+    bio: "My bio.",
     commits: 0,
     issues: 0,
     unit_tests: 0,
   },
   {
-    name: "Faiza Rahman",
-    gitlab_username: "faiza2002",
-    email: "faiza2002@gmail.com",
-    image: FaizaAvatar,
-    role: "Front-end",
-    bio: "I'm a third-year CS major at UT Austin. I'm mostly interested in contributing to the front-end work of this project as I have worked in the past only on the backend. In my freetime, I like to watch movies with friends or cook new recipes.",
+    name: "Peyton Ausburn",
+    gitlab_username: "peausburn",
+    email: "email",
+    // image: PeytonAvatar,
+    role: "Fullstack",
+    bio: "My bio.",
     commits: 0,
     issues: 0,
     unit_tests: 0,
   },
   {
-    name: "Mikala Jaramillo",
-    gitlab_username: "Mikalajj",
-    email: "jaramillomikala@utexas.edu",
-    image: MikalaAvatar,
-    role: "Front-end",
-    bio: "I am a Senior at the University of Texas and I will be graduating in the fall with a Bachelor’s in Computer Science. I enjoy playing golf as one of my hobbies.",
+    name: "Peter Hwang",
+    gitlab_username: "petersh0317",
+    email: "petersh0317@utexas.edu",
+    // image: PeterAvatar,
+    role: "Fullstack",
+    bio: "My bio.",
     commits: 0,
     issues: 0,
     unit_tests: 0,
   },
   {
-    name: "Sarthak Sirotiya",
-    gitlab_username: "sarthaksirotiya",
-    email: "sarthakssi2@gmail.com",
-    image: SarthakAvatar,
-    role: "AWS & Front-end",
-    bio: "I'm a third year CS major at UT Austin, and I'm from Colleyville, Texas. I enjoy skiing (I've done it exactly once), playing racquetball, and cooking.",
+    name: "Niyati Prabhu",
+    gitlab_username: "niyatiprabhu",
+    email: "email",
+    // image: NiyatiAvatar,
+    role: "Fullstack",
+    bio: "My bio.",
     commits: 0,
     issues: 0,
     unit_tests: 0,
   },
   {
-    name: "Thomas Langford",
-    gitlab_username: "thomas_langford",
-    email: "thomaslangford0123@gmail.com",
-    image: ThomasAvatar,
-    role: "Back-end",
-    bio: "I'm a third year computer science major at the University of Texas at Austin. I enjoy playing the double bass and mountain biking.",
+    name: "Adrian Sanchez",
+    gitlab_username: "adriansacas",
+    email: "adriansc91@utexas.edu",
+    // image: AdrianAvatar,
+    role: "Fullstack",
+    bio: "My bio.",
     commits: 0,
     issues: 0,
     unit_tests: 0,
--- my-app/src/views/About.js
+++ my-app/src/views/About.js
@@ -1,10 +1,188 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import DeveloperCard from "../components/Cards/DeveloperCard";
+import { teamInfo } from "../static/TeamInfo";
+import { toolInfo, apiInfo } from "../static/ProjectInfo";
+
+import Spinner from "react-bootstrap/Spinner";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Stack from "react-bootstrap/Stack";
+import ToolCard from "../components/Cards/ToolCard";
+import APICard from "../components/Cards/APICard";
+
+const client = axios.create({
+    baseURL: "https://gitlab.com/api/v4/",
+});
+
+const fetchGitLabData = async () => {
+    let totalCommits = 0,
+        totalIssues = 0,
+        totalUnitTests = 0;
+
+    teamInfo.forEach((member) => {
+        member.commits = 0;
+        member.issues = 0;
+        totalUnitTests += member.unit_tests;
+    });
+
+    await client
+        .get("projects/43357786/repository/contributors")
+        .then((response) => {
+            response.data.forEach((element) => {
+                const { name, email, commits } = element;
+
+                teamInfo.forEach((member) => {
+                    if (
+                        member.name === name ||
+                        member.gitlab_username === name ||
+                        member.email === email
+                    ) {
+                        console.log(commits);
+                        member.commits += commits;
+                    }
+                });
+                totalCommits += commits;
+            });
+        });
+
+    await client.get("projects/43357786/issues").then((response) => {
+        response.data.forEach((element) => {
+            const { assignees } = element;
+            assignees.forEach((assignee) => {
+                const { name, email } = assignee;
+                teamInfo.forEach((member) => {
+                    if (
+                        member.name === name ||
+                        member.gitlab_username === name ||
+                        member.email === email
+                    )
+                        member.issues += 1;
+                });
+            });
+            totalIssues += 1;
+        });
+    });
+
+    console.log("Members");
+    teamInfo.forEach((member) => {
+        console.log(member.issues);
+    });
+
+    return {
+        totalCommits: totalCommits,
+        totalIssues: totalIssues,
+        totalTests: totalUnitTests,
+        teamInfo: teamInfo,
+    };
+};
 
 const About = () => {
+    const [teamList, setTeamList] = useState([]);
+    const [totalCommits, setTotalCommits] = useState(0);
+    const [totalIssues, setTotalIssues] = useState(0);
+    const [totalTests, setTotalTests] = useState(0);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (teamList === undefined || teamList.length === 0) {
+                const gitlabInfo = await fetchGitLabData();
+                setTotalCommits(gitlabInfo.totalCommits);
+                setTotalIssues(gitlabInfo.totalIssues);
+                setTotalTests(gitlabInfo.totalTests);
+                setTeamList(gitlabInfo.teamInfo);
+                setLoaded(true);
+            }
+        };
+        fetchData();
+    }, [teamList]);
+
     return (
-        <div>
-            <h1>About Page</h1>
-        </div>
+        <Stack>
+            <Container className="p-4">
+                <h1 className="d-flex justify-content-center p-4 ">What is CloseReading?</h1>
+                <p className="mx-auto">
+                    CloseReading is uh... something for books and libraries.
+                </p>
+            </Container>
+            <Container className="p-4 bg-dark text-light">
+                <h1 className="d-flex justify-content-center p-4 ">
+                    How It Works
+                </h1>
+                <p className="mx-auto">
+                    We took some stuff and integrated it with other stuff to make a bigger stuff.
+                </p>
+            </Container>
+            <Container className="p-4">
+                <h1 className="d-flex justify-content-center p-4 ">Meet the Team!</h1>
+                {loaded ? (
+                    <Row
+                        xs={1}
+                        sm={2}
+                        md={3}
+                        xl={5}
+                        className="g-4 p-4 justify-content-center"
+                    >
+                        {teamList.map((member) => {
+                            return (
+                                <Col>
+                                    <DeveloperCard devInfo={member} />
+                                </Col>
+                            );
+                        })}
+                    </Row>
+                ) : (
+                    <Row>
+                        <Col className="d-flex justify-content-center">
+                            <Spinner animation="grow" />
+                        </Col>
+                    </Row>
+                )}
+            </Container>
+            <Container className="p-4 bg-dark text-light">
+                <h1 className="d-flex justify-content-center p-4">
+                    Total Repository Stats
+                </h1>
+                <Row className="p-4">
+                    <Col className="d-flex justify-content-center">
+                        <h2>Total Commits: {totalCommits}</h2>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <h2>Total Issues: {totalIssues}</h2>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <h2>Total Tests: {totalTests}</h2>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="p-4">
+                <h1 className="d-flex justify-content-center p-4 ">Tools</h1>
+                <Row xs={2} md={4} className="g-4 justify-content-center">
+                    {toolInfo.map((tool) => {
+                        return (
+                            <Col>
+                                <ToolCard toolInfo={tool} />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+            <Container className="p-4">
+                <h1 className="d-flex justify-content-center p-4 ">APIs</h1>
+                <Row xs={1} sm={2} md={4} className="g-4 justify-content-center">
+                    {apiInfo.map((api) => {
+                        return (
+                            <Col>
+                                <APICard apiInfo={api} />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+        </Stack>
     );
 };
 
