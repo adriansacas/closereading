7888321ce6431a0354049581fc39fe0133c13774
Peyton Ausburn <peytonausburn@Leslies-MacBook-Pro-4.local>
2023-03-08 16:11:37 -0600

    fix routes in cards

--- my-app/src/components/Cards/AuthorCard.js
+++ my-app/src/components/Cards/AuthorCard.js
@@ -15,7 +15,7 @@ const AuthorCard = (props) => {
         </Card.Body>
         <Card.Footer className="text-muted">
           <li>
-            <Link to={`/author/`+id}>More info</Link>
+            <Link to={`/authors/`+id}>More info</Link>
           </li>
         </Card.Footer>
       </Card>
