fc4c09dde1d1e6ce20e9d0054dd78f03736b3a2f
niyatiprabhu <niyatiprabhu@utexas.edu>
2023-03-09 00:33:05 +0000

    Revert "Merge branch 'api-scraping' into 'main'"
    This reverts merge request !13

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-user = os.environ['DATABASE_USERNAME']
-password = os.environ['DATABASE_PASSWORD']
-host = os.environ['DATABASE_HOSTNAME']
-database = os.environ['DATABASE_NAME']
-port = os.environ['DATABASE_PORT']
-
-SQLALCHEMY_DATABASE_URI = f'postgresql://{user}:{password}@{host}:{port}/{database}'
