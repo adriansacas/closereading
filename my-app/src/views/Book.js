030157579e6058074ab42731198c293ca0fb5e23
Adrian Sanchez <adriansc91@utexas.edu>
2023-03-09 20:12:32 -0600

    Added axios client to get api data. Added CORS support. Modified populate db to include thumbnails

--- my-app/src/views/Book.js
+++ my-app/src/views/Book.js
@@ -1,41 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {Link, useParams} from "react-router-dom";
-import bookData from "../Books.json"
+// import book from "../Books.json"
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import libraryData from "../Libraries.json";
+// import libraryData from "../Libraries.json";
 import LibraryCard from "../components/Cards/LibraryCard";
+import Spinner from "react-bootstrap/Spinner";
+
+
+const client = axios.create({
+    baseURL: "http://localhost:4000",
+});
+
 
 const Book = () => {
     const { id } = useParams();
+    const [book, setBook] = useState();
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        const getBook = async () => {
+            if (book === undefined) {
+                await client
+                    .get(`books/${id}`)
+                    .then((response) => {
+                        setBook(response.data["data"]);
+                    })
+                    .catch((err) => console.log(err));
+            }
+            setLoaded(true);
+            console.log(book);
+        };
+        getBook();
+    }, [book]);
+
     return (
         <Container fluid>
-            <Row>
-                <Col>
-                    <h1 className="d-flex justify-content-center p-4 ">{bookData[id - 1].name}</h1>
-                    <div>Author: <Link to={`/authors/`+id}>{bookData[id - 1].author}</Link></div>
-                    <div>Pages: {bookData[id - 1].number_of_pages}</div>
-                    <div>Year: {bookData[id - 1].publishing_year}</div>
-                    <div>Publisher: {bookData[id - 1].publisher}</div>
-                    <div>NYT Best-Seller: {bookData[id - 1].NYT_best_seller}</div>
-                    <h5>Description:</h5>
-                    <div>{bookData[id -1].description}</div>
-                    <h5>Libraries</h5>
-                    <Row md={3} className="p-4 g-4 justify-content-center">
-                        {libraryData.map((library) => {
-                            return (
-                                <Col>
-                                    <LibraryCard libraryData={library} />
-                                </Col>
-                            );
-                        })}
-                    </Row>
-                </Col>
-                <Col>
-                    <img src={bookData[id -1].cover} alt="Book cover."/>
-                </Col>
-            </Row>
+        {loaded ? (
+                <Row>
+                    <Col>
+                        <h1 className="d-flex justify-content-center p-4 ">{book[0].title}</h1>
+                        {/*<div>Author: <Link to={`/authors/`+id}>{book[id - 1].author}</Link></div>*/}
+                        <div>Pages: {book[0].page_count}</div>
+                        <div>Year: {book[0].pub_year}</div>
+                        <div>Genre: {book[0].genre}</div>
+                        {/*<div>Publisher: {book[0].publisher}</div>*/}
+                        {/*<div>NYT Best-Seller: {book[id - 1].NYT_best_seller}</div>*/}
+                        <h5>Description:</h5>
+                        <div>{book[0].description}</div>
+                        <h5>Libraries</h5>
+                        {/*<Row md={3} className="p-4 g-4 justify-content-center">*/}
+                        {/*    {libraryData.map((library) => {*/}
+                        {/*        return (*/}
+                        {/*            <Col>*/}
+                        {/*                <LibraryCard libraryData={library} />*/}
+                        {/*            </Col>*/}
+                        {/*        );*/}
+                        {/*    })}*/}
+                        {/*</Row>*/}
+                    </Col>
+                    <Col>
+                        <img src={book[0].image_url} alt="Book cover."/>
+                    </Col>
+                </Row>
+            ) : (
+                <Spinner animation="grow" />
+        )}
         </Container>
     );
 };
