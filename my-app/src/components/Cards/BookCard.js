2b75d95660014047e62c8702ce515970405e26e8
Sumaya Al-Bedaiwi <sumaya@wireless-10-155-196-37.public.utexas.edu>
2023-03-09 23:07:52 -0600

    framework for pagination - not working

--- my-app/src/components/Cards/BookCard.js
+++ my-app/src/components/Cards/BookCard.js
@@ -3,7 +3,7 @@ import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
 const BookCard = (props) => {
-  const { name, author, number_of_pages, publishing_year, publisher, NYT_best_seller, id } = props.bookData;
+  const {name, author, number_of_pages, publishing_year, genre, id} = props.bookData;
   return (
       <Card border="dark">
         <Card.Body>
@@ -11,8 +11,8 @@ const BookCard = (props) => {
           <Card.Text>Author: {author}</Card.Text>
           <Card.Text>Pages: {number_of_pages}</Card.Text>
           <Card.Text>Year: {publishing_year}</Card.Text>
-          <Card.Text>Publisher: {publisher}</Card.Text>
-          <Card.Text>NYT Best-Seller: {NYT_best_seller}</Card.Text>
+          <Card.Text>Genre: {genre}</Card.Text>
+          {/* <Card.Text>NYT Best-Seller: {NYT_best_seller}</Card.Text> */}
         </Card.Body>
         <Card.Footer className="text-muted">
           <li>
--- my-app/src/views/Books.js
+++ my-app/src/views/Books.js
@@ -4,25 +4,188 @@ import Stack from "react-bootstrap/Stack";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import bookData from "../Books.json"
+// import bookData from "../Books.json"
+import Pagination from "react-bootstrap/Pagination";
+import axios from "axios"
+import { useState, useEffect } from "react"
+import Spinner from "react-bootstrap/Spinner";
 
+const client = axios.create({
+    baseURL: "http://localhost:4000/",
+  });
+  
+  const PER_PAGE = 20
+  const NUM_BOOKS = 304
+    
 
 const Books = () => {
+    const [books, setBooks] = useState([])
+    const [loaded, setLoaded] = useState(false)
+    const [activePage, setActivePage] = useState(1);
+
+    function handleClick(num) {
+        console.log("clicked page", num)
+        setActivePage(num)
+        setLoaded(false)
+    }
+
+      useEffect(() => {
+        const fetchBooks = async() => {
+            if (!loaded) {
+                await client
+                  .get(`books?page=${activePage}`)
+                  .then((response) => {setBooks(response.data["data"])})
+                  .catch((err) => console.log(err));
+                setLoaded(true);
+            }
+        };
+        fetchBooks();
+    }, [books, loaded]);
+
+    let numPages = Math.ceil(NUM_BOOKS / PER_PAGE)
+    let items = []
+    for (let num = activePage - 2; num <= activePage + 2; num++) {
+        if (num > 0 && num <= numPages) {
+            items.push(
+                <Pagination.Item
+                    key={num}
+                    onClick={() => handleClick(num)}
+                    active={num === activePage}>
+                        {num}
+                </Pagination.Item>
+            )
+        }
+    }
+
+    
+
     return (
-        <Stack>
-            <Container className="p-4">
-                <h1 className="d-flex justify-content-center p-4 ">Books</h1>
-                <Row md={10} className="p-4 g-4 justify-content-center">
-                    {bookData.map((book) => {
-                        return (
-                            <Col>
-                                <BookCard bookData={book} />
-                            </Col>
-                        );
-                    })}
+
+        <Container className="p-4">
+            <h1 className="d-flex justify-content-center p-4 ">Books</h1>
+
+            {/* Pagination */}
+            <Pagination className="justify-content-center">
+                {activePage > 3 && (
+                <Pagination.First
+                    key={1}
+                    onClick={() => handleClick(1)}
+                    active={1 === activePage}>
+                    1
+                </Pagination.First>
+                )}
+                {activePage > 4 && <Pagination.Ellipsis/>}
+                {items}
+                {activePage < numPages - 3 && <Pagination.Ellipsis />}
+                {activePage < numPages - 2 && (
+                <Pagination.Last
+                    key={numPages}
+                    onClick={() => handleClick(numPages)}
+                    active={numPages === activePage}>
+                    {numPages}
+                </Pagination.Last>
+                )}
+            </Pagination>
+
+            {/* Card Grid */}
+            {/* <p>Showing {books.length} out of 282 instances</p> */}
+
+            <Container style={{display: 'flex'}}>
+                <Row
+                xl={5}
+                lg={4}
+                md={3}
+                sm={2}
+                xs={1}
+                className="d-flex g-0 p-0 justify-content-center">
+                    { loaded ? (
+                        books["data"].map((bookData) => {
+                            return ( 
+                                <Col key={bookData.id} className = "d-flex align-self-stretch">
+                                    <BookCard bookData={bookData}/>
+                                       
+                                </Col>
+                            )
+                        })) : (<Spinner animation="grow"/>)}
                 </Row>
             </Container>
-        </Stack>
+
+            {/* Pagination */}
+            <Pagination className="justify-content-center py-3">
+                {activePage > 3 && (
+                <Pagination.First
+                    key={1}
+                    onClick={() => handleClick(1)}
+                    active={1 === activePage}>
+                    1
+                </Pagination.First>
+                )}
+                {activePage > 4 && <Pagination.Ellipsis/>}
+                {items}
+                {activePage < numPages - 3 && <Pagination.Ellipsis />}
+                {activePage < numPages - 2 && (
+                <Pagination.Last
+                    key={numPages}
+                    onClick={() => handleClick(numPages)}
+                    active={numPages === activePage}>
+                    {numPages}
+                </Pagination.Last>
+                )}
+            </Pagination>
+            
+    </Container>
+
+
+// id = {book.id}
+// name = {book.title} 
+// author = {book.author_id}
+// number_of_pages = {book.page_count}
+// publishing_year = {book.pub_year} 
+// genre = {book.genre}/>
+
+        // // <Stack>
+        //     <Container className="p-4">
+        //         <h1 className="d-flex justify-content-center p-4 ">Books</h1>
+
+
+        //         {/* Pagination */}
+        //         <Pagination className="justify-content-center">
+        //             {activePage > 3 && (
+        //             <Pagination.First
+        //                 key={1}
+        //                 onClick={() => handleClick(1)}
+        //                 active={1 === activePage}>
+        //                 1
+        //             </Pagination.First>
+        //             )}
+        //             {activePage > 4 && <Pagination.Ellipsis/>}
+        //             {items}
+        //             {activePage < numPages - 3 && <Pagination.Ellipsis />}
+        //             {activePage < numPages - 2 && (
+        //             <Pagination.Last
+        //                 key={numPages}
+        //                 onClick={() => handleClick(numPages)}
+        //                 active={numPages === activePage}>
+        //                 {numPages}
+        //             </Pagination.Last>
+        //             )}
+        //         </Pagination>
+
+
+
+
+
+        //         <Row md={10} className="p-4 g-4 justify-content-center">
+        //             {books.map((data) => {
+        //                 return (
+        //                     <Col>
+        //                         <BookCard books={book} />
+        //                     </Col>
+        //                 );
+        //             })}
+        //         </Row>
+        //     </Container>
+        // // </Stack>
     );
 };
 
