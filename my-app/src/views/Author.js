71f64f4ab6e4c4ff64be1887d7f3e5dc87dad220
Adrian Sanchez <adriansc91@utexas.edu>
2023-03-09 21:44:51 -0600

    Added api requests to author view

--- my-app/src/views/Author.js
+++ my-app/src/views/Author.js
@@ -1,43 +1,71 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
-import authorData from "../Authors.json"
-import bookData from "../Books.json"
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import BookCard from "../components/Cards/BookCard";
+// import BookCard from "../components/Cards/BookCard";
+import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
+
+
+const client = axios.create({
+    baseURL: "http://localhost:4000",
+});
+
 
 const Author = () => {
     const { id } = useParams();
+    const [author, setAuthor] = useState();
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        const getAuthor = async () => {
+            if (author === undefined) {
+                await client
+                    .get(`authors/${id}`)
+                    .then((response) => {
+                        setAuthor(response.data["data"]);
+                    })
+                    .catch((err) => console.log(err));
+            }
+            setLoaded(true);
+        };
+        getAuthor();
+    }, [author]);
+
     return (
         <Container fluid>
-            <Row>
-                <Col>
-                    <img src={authorData[id -1].portrait} alt="Author's portrait"/>
-                </Col>
-                <Col>
-                    <h1 className="d-flex justify-content-center p-4 ">{authorData[id - 1].name}</h1>
-                    <div>Age: {authorData[id - 1].age}</div>
-                    <div>Nationality: {authorData[id - 1].nationality}</div>
-                    <div>Gender: {authorData[id - 1].gender}</div>
-                    <div>Number of publications: {authorData[id - 1].number_of_publications}</div>
-                    <h5>Biography:</h5>
-                    <div>{authorData[id -1].biography}</div>
-                    <h5>Books</h5>
-                    <Row md={2} className="p-4 g-4 justify-content-center">
-                        {bookData.map((book) => {
-                            if (book.id === authorData[id - 1].books[0]) {
-                                return (
-                                    <Col>
-                                        <BookCard bookData={book}/>
-                                    </Col>
-                                );
-                            }
-                            return null;
-                        })}
-                    </Row>
-                </Col>
-            </Row>
+            {loaded ? (
+                <Row>
+                    <Col>
+                        <img src={author[0].image_url} alt="Author's portrait"/>
+                    </Col>
+                    <Col>
+                        <h1 className="d-flex justify-content-center p-4 ">{author[0].name}</h1>
+                        {/*<div>Age: {author[0].age}</div>*/}
+                        {/*<div>Nationality: {author[0].nationality}</div>*/}
+                        {/*<div>Gender: {author[0].gender}</div>*/}
+                        {/*<div>Number of publications: {author[0].number_of_publications}</div>*/}
+                        <h5>Biography:</h5>
+                        <div>{author[0].bio}</div>
+                        <h5>Books</h5>
+                        {/*<Row md={2} className="p-4 g-4 justify-content-center">*/}
+                        {/*    {bookData.map((book) => {*/}
+                        {/*        if (book.id === author[0].books[0]) {*/}
+                        {/*            return (*/}
+                        {/*                <Col>*/}
+                        {/*                    <BookCard bookData={book}/>*/}
+                        {/*                </Col>*/}
+                        {/*            );*/}
+                        {/*        }*/}
+                        {/*        return null;*/}
+                        {/*    })}*/}
+                        {/*</Row>*/}
+                    </Col>
+                </Row>
+            ) : (
+                <Spinner animation="grow" />
+            )}
         </Container>
     );
 };
