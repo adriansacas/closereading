84a2d54746520deda99a7241b2b8c6d03b98c107
Adrian Sanchez <adriansc91@utexas.edu>
2023-03-15 17:25:24 -0500

    Include pagination data in API response

--- backend/schema.py
+++ backend/schema.py
@@ -1,28 +1,28 @@
 from models import Book, Author, Library
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from marshmallow import fields
+from marshmallow import fields, post_dump
 
 
 class AuthorSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Author
-        include_relationships = True
 
     # Fetches the data from the one-to-many relationship and nests it within
     books = fields.List(fields.Nested('BookSchema', exclude=('author',)))
 
+
 class BookSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Book
-        # Will include the foreign key of the relationship
-        include_relationships = True
 
     # Fetches the data from the relationship and nests it within
     author = fields.Nested('AuthorSchema', only=('id', 'name'))
 
+
 class LibrarySchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Library
+
 
 book_schema = BookSchema()
 author_schema = AuthorSchema()
